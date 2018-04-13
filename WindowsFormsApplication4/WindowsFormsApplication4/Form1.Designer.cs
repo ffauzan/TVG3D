e23efbd43d64db715902e04baefc31e1a53d87ee
Falih Fauzan <32434534+ffauzan@users.noreply.github.com>
2018-04-13 21:08:12 +0700

    Add files via upload
    yttuytuyuy

---
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl1 = new OpenTK.GLControl();
+            this.BeginX = new System.Windows.Forms.TextBox();
+            this.BeginY = new System.Windows.Forms.TextBox();
+            this.BeginZ = new System.Windows.Forms.TextBox();
+            this.EndX = new System.Windows.Forms.TextBox();
+            this.EndZ = new System.Windows.Forms.TextBox();
+            this.EndY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(12, 12);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(917, 394);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            this.glControl1.Resize += new System.EventHandler(this.glControl1_Resize);
+            // 
+            // BeginX
+            // 
+            this.BeginX.Location = new System.Drawing.Point(45, 430);
+            this.BeginX.Name = "BeginX";
+            this.BeginX.Size = new System.Drawing.Size(31, 20);
+            this.BeginX.TabIndex = 1;
+            this.BeginX.TextChanged += new System.EventHandler(this.EndY_TextChanged);
+            // 
+            // BeginY
+            // 
+            this.BeginY.Location = new System.Drawing.Point(79, 430);
+            this.BeginY.Name = "BeginY";
+            this.BeginY.Size = new System.Drawing.Size(31, 20);
+            this.BeginY.TabIndex = 2;
+            this.BeginY.TextChanged += new System.EventHandler(this.EndY_TextChanged);
+            // 
+            // BeginZ
+            // 
+            this.BeginZ.Location = new System.Drawing.Point(116, 430);
+            this.BeginZ.Name = "BeginZ";
+            this.BeginZ.Size = new System.Drawing.Size(31, 20);
+            this.BeginZ.TabIndex = 3;
+            this.BeginZ.TextChanged += new System.EventHandler(this.EndY_TextChanged);
+            // 
+            // EndX
+            // 
+            this.EndX.Location = new System.Drawing.Point(45, 459);
+            this.EndX.Name = "EndX";
+            this.EndX.Size = new System.Drawing.Size(31, 20);
+            this.EndX.TabIndex = 4;
+            this.EndX.TextChanged += new System.EventHandler(this.EndY_TextChanged);
+            // 
+            // EndZ
+            // 
+            this.EndZ.Location = new System.Drawing.Point(116, 459);
+            this.EndZ.Name = "EndZ";
+            this.EndZ.Size = new System.Drawing.Size(31, 20);
+            this.EndZ.TabIndex = 6;
+            this.EndZ.TextChanged += new System.EventHandler(this.EndY_TextChanged);
+            // 
+            // EndY
+            // 
+            this.EndY.Location = new System.Drawing.Point(79, 459);
+            this.EndY.Name = "EndY";
+            this.EndY.Size = new System.Drawing.Size(31, 20);
+            this.EndY.TabIndex = 7;
+            this.EndY.TextChanged += new System.EventHandler(this.EndY_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(49, 409);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Garis Rotasi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 433);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 462);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "B";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 485);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Update Garis";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(626, 485);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "-->";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(528, 485);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "<--";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(606, 459);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 17);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 520);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EndY);
+            this.Controls.Add(this.EndZ);
+            this.Controls.Add(this.EndX);
+            this.Controls.Add(this.BeginZ);
+            this.Controls.Add(this.BeginY);
+            this.Controls.Add(this.BeginX);
+            this.Controls.Add(this.glControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.TextBox BeginX;
+        private System.Windows.Forms.TextBox BeginY;
+        private System.Windows.Forms.TextBox BeginZ;
+        private System.Windows.Forms.TextBox EndX;
+        private System.Windows.Forms.TextBox EndZ;
+        private System.Windows.Forms.TextBox EndY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication4/WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        bool loaded = false;
+        int beginX, beginY, beginZ, endX, endY, endZ;
+        Vector3 Begin, End, p, q, r, s, q2, r2, s2, p2;
+        float angle = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void updateRotLine()
+        {
+            beginX = Convert.ToInt32(BeginX.Text);
+            beginY = Convert.ToInt32(BeginY.Text);
+            beginZ = Convert.ToInt32(BeginZ.Text);
+
+            endX = Convert.ToInt32(EndX.Text);
+            endY = Convert.ToInt32(EndY.Text);
+            endZ = Convert.ToInt32(EndZ.Text);
+
+            p = new Vector3(50, 30, -5);
+            q = new Vector3(55, 25, -10);
+            r = new Vector3(55, 25, 0);
+            s = new Vector3(45, 25, -5);
+
+            Begin = new Vector3(beginX, beginY, beginZ);
+            End = new Vector3(endX, endY, endZ);
+        }
+
+       
+
+        private Vector3 RotasiY(Vector3 titik, double sudut)
+        {
+            double cosx = Math.Cos(sudut);              
+            double sinx = Math.Sin(sudut);
+
+            double newx = (titik.X * cosx) - (titik.Z * sinx);
+            double newz = (titik.X * sinx) + (titik.Z * cosx);
+
+            titik.X = (float)newx;
+            titik.Z = (float)newz;
+            return titik;
+        }
+
+        private Vector3 RotasiX(Vector3 titik, double sudut)
+        {
+            double cosx = Math.Cos(sudut);              //rotasi terhadap sumbu X
+            double sinx = Math.Sin(sudut);
+
+            double newz = (titik.Z * cosx) - (titik.Y * sinx);
+            double newy = (titik.Z * sinx) + (titik.Y * cosx);
+
+            titik.Z = (float)newz;
+            titik.Y = (float)newy;
+            return titik;
+        }
+
+        private Vector3 RotasiZ(Vector3 titik, double sudut)
+        {
+            double cosx = Math.Cos(sudut);              //rotasi terhadap sumbu Z
+            double sinx = Math.Sin(sudut);
+
+            double newx = (titik.X * cosx) - (titik.Y * sinx);
+            double newy = (titik.X * sinx) + (titik.Y * cosx);
+
+            titik.X = (float)newx;
+            titik.Y = (float)newy;
+            return titik;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            angle -= 5;
+            glControl1.Invalidate();
+            label4.Text = angle.ToString();
+        }
+
+        private Vector3 rotasi(float sudutt, Vector3 point)
+        {
+            
+            point -= End;
+            Vector3 AB = End - Begin;
+
+
+            double sudut1 = Math.Atan(AB.Y / AB.Z);
+
+            AB = RotasiX(AB, sudut1);
+
+
+            point = RotasiX(point, sudut1);
+
+
+            double sudut2 = Math.Atan(AB.X / AB.Z);
+
+            point = RotasiY(point, sudut2);
+
+
+            double sudut3 = (Math.PI / 180) * sudutt;
+
+            point = RotasiZ(point, -sudut3);
+
+
+            point = RotasiY(point, -sudut2);
+
+            point = RotasiX(point, -sudut1);
+
+            point += End;                  
+
+            return point;
+
+        }
+
+
+        private void DrawRotLine(Vector3 begin, Vector3 end)
+        {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Brown);
+            GL.Vertex3(begin);
+            GL.Vertex3(end);
+            GL.End();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            angle += 5;
+            glControl1.Invalidate();
+            label4.Text = angle.ToString();
+        }
+
+       
+
+        private void drawObject2()
+        {
+            
+            GL.Begin(PrimitiveType.LineLoop);
+            GL.Color3(Color.DarkOrchid);
+            GL.Vertex3(p2);
+            GL.Vertex3(q2);
+            GL.Vertex3(r2);
+            GL.Vertex3(s2);
+            GL.Vertex3(p2);
+            GL.Vertex3(r2);
+            GL.Vertex3(q2);
+            GL.Vertex3(s2);
+            GL.End();
+            
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded) // Play nice
+                return;
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            
+
+            SetupCoordinate();
+
+            DrawRotLine(Begin, End);
+
+            p2 = rotasi(angle, p);
+            q2 = rotasi(angle, q);
+            r2 = rotasi(angle, r);
+            s2 = rotasi(angle, s);
+
+            drawObject2();
+
+            glControl1.SwapBuffers();
+        }
+
+        private void EndY_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            updateRotLine();
+            glControl1.Invalidate();
+        }
+
+        private void SetupCoordinate()
+        {
+            GL.Rotate(-30.0, 1, 0, 0);
+            GL.Rotate(135.0, 0, 1, 0);
+            GL.Scale(0.01, 0.01, 0.01);
+            GL.Translate(0, -20, 0);
+
+
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Red);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(100, 0, 0);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Green);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 100, 0);
+            GL.End();
+
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Blue);
+            GL.Vertex3(0, 0, 0);
+            GL.Vertex3(0, 0, 100);
+            GL.End();
+        }
+
+       
+
+        private void SetupViewport()
+        {
+            int w = glControl1.Width;
+            int h = glControl1.Height;
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, w, 0, h, -100, 100); // Bottom-left corner pixel has coordinate (0, 0)
+            GL.Viewport(0, 0, w, h); // Use all of the glControl painting area
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+            GL.ClearColor(Color.SkyBlue);
+            //SetupViewport();
+            
+            
+
+            GL.Rotate(-30.0, 1, 0, 0);
+            GL.Rotate(135.0, 0, 1, 0);
+        }
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            if (!loaded)
+                return;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BeginX.Text = "10";
+            BeginY.Text = "20";
+            BeginZ.Text = "5";
+
+            EndX.Text = "15";
+            EndY.Text = "40";
+            EndZ.Text = "0";
+           
+
+        }
+    }
+}
